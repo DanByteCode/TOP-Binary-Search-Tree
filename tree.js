9ce0a925273a35bd34b111b251781273ca0e5e63
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-03-23 19:05:44 -0400

    Adding getRoot function

--- tree.js
+++ tree.js
@@ -1,6 +1,7 @@
 import { Node } from "./node-tree.js";
 export function Tree(array = []) {
     let root = null
+    const getRoot = ()=>{return root}
     buildTree(array)
 
     function buildTree(array) {
@@ -167,6 +168,7 @@ export function Tree(array = []) {
     }
     return {
         root,
+        getRoot,
         buildTree,
         insert,
         deleteItem,
